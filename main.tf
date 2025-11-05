ff6fc88e9326975076e03fa8794deb4dc744c7f7
AmalanSuresh <109796428+AmalanSuresh@users.noreply.github.com>
2025-11-05 16:21:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "eu-north-1"
 }
 resource "aws_instance" "one" {
-ami = "ami-0844f359572fd681d"
+ami = "ami-0ea856cd0a5117fa8"
 instance_type = "t3.micro"
 tags = {
 Name = "amalan-server"
